1621e3bc3f94188cde7336e7232025f1d0eba73d
Arnaud ZOBEC <arnozobec@gmail.com>
2015-07-18 21:40:56 +0200

    correct parsexml module

--- Analysis_Raspberry/bin/modules/FIR.py
+++ Analysis_Raspberry/bin/modules/FIR.py
@@ -43,6 +43,45 @@ def create_event(configurations):
         pass
 
 
+def create_event_from_xml(configurations,datas):
+    url="http://"+configurations["FirIP"]+":8000/events/new/"
+    data= {
+        'csrfmiddlewaretoken': 'JrDSAkAfHh5kwA7UERzchoDI0RVR10pZ',
+        'subject': 'test ARNAUD ZOBEC 2',
+        'category': '1',
+        'status': 'O',
+        'detection': '2',
+        'severity': '1',
+        'date': '18/07/2015 16:01:04',
+        'actor': '',
+        'plan': '',
+        'confidentiality': '1',
+        'description': '<p>bonjour</p>',
+        }
+    cookies = {'sessionid' : 'umd71fiphl7hq15sr3cvee9x1ljjql9w',
+                'csrftoken' : 'JrDSAkAfHh5kwA7UERzchoDI0RVR10pZ',
+                }
+    headers = {'Accept-Language' : 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3' }
+
+    cookies2 = dict (sessionid = 'umd71fiphl7hq15sr3cvee9x1ljjql9w',
+        csrftoken = 'JrDSAkAfHh5kwA7UERzchoDI0RVR10pZ' )
+
+    try:
+        r = requests.post(url, data=data, cookies=cookies2, headers=headers)
+        print r.status_code
+        if r.status_code == requests.codes.ok:
+            print("Event Created")
+        else:
+            print("Erreur lors de la création (cookie?)")
+    except requests.exceptions.Timeout:
+        print(">>> Requete TimeOut")
+    except requests.exceptions.TooManyRedirects:
+        print(">>> TooManyRedirects")
+    except requests.exceptions.RequestException:
+        pass
+
+
+
 if __name__ == '__main__':
     configurations = parse_configuration_file("../../etc/Analysis_Sender.conf")
     create_event(configurations)
--- Analysis_Raspberry/bin/modules/parsexml.py
+++ Analysis_Raspberry/bin/modules/parsexml.py
@@ -5,12 +5,19 @@ from lxml import etree
 
 def honeypot_proofs(configurations,proof_file):
 	tree = etree.parse(configurations["DataLocation"]+proof_file)
-	for preuve in tree.xpath("/preuves/preuve"):
+	for preuve in tree.xpath("/rapport/preuves/preuve"):
 		
 		if preuve.get("type") == "tentative de connexion":
 			print("###### Preuve ######")
 			print("IP :"+preuve.find("ip").text)
 			print("PORT:"+preuve.find("port").text)
 			print("Data:"+preuve.find("data").text)
+
+			#IF NIVEAU 3 : ON ENVOIE
+			if preuve.find("score").text == "3":
+				print("Preuve niveau 3 :!!")
+
+			#IF NIVEAU 4 : ON ENVOIE AUSSI
+
 
 #ID  for THREATINTEL is 18
